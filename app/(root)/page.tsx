34ad78d00bc8b7339a45ac5e6200685cdf0fa5ab
AbuBakkarSiddiqueAnir <anirdms@gmail.com>
2023-11-23 15:03:16 +0600

    :racehorse: zustand added with useModalStore hook

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 export default function SetupPage() {
-  return <div className="p-3"></div>;
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  });
+  return <div className="p-3">Root page</div>;
 }
